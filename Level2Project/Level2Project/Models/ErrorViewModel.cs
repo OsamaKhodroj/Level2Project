f8b99d21fcf8a620d40ab6486ec97e7642a4cfe7
Osama Khodrog <khodrog@gmail.com>
2025-06-23 18:55:27 +0300

    create a new pull code

---
+++ Level2Project/Level2Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Level2Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
